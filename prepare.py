d6c1332386f01d006a359bea9a4dec3c8b1726fc
AmrMKayid <amrkayid2027@gmail.com>
2019-09-02 15:37:08 +0200

    Add prepared data

---
+++ prepare.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+from collections import defaultdict, Counter
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+KAYMODB_PATH = f'{PATH}/datasets/kaymodb/'
+
+def prepare_emodb():
+    emodb_path = f'{PATH}/datasets/emodb/wav/'
+    wav_data = os.listdir(emodb_path)
+    
+    EMOTIONS = {
+        'W': 'angry',
+        'L': 'bored',
+        'E': 'disgust',
+        'A': 'fearful',
+        'F': 'happy',
+        'T': 'sad',
+        'N': 'neutral',
+    }
+    emotion_count = Counter(EMOTIONS.values())
+    print(emotion_count)
+
+    wav_file_path = []
+
+    for wav_file in wav_data:
+        if not wav_file.endswith('wav'):
+            continue
+        wav_info, ext = wav_file.split('.')
+        # print(wav_info)
+        speaker_num, text, emotion, extra = wav_info[:2], wav_info[2:5], wav_info[5], wav_info[6]
+        # print(speaker_num, text, emotion, extra)
+        emotion_path = KAYMODB_PATH + EMOTIONS[emotion]
+        # print(emotion_path)
+        os.makedirs(emotion_path, exist_ok=True)
+
+        # print(emodb_path + wav_file, emotion_path + f'/0_emodb_{EMOTIONS[emotion]}_{emotion_count[EMOTIONS[emotion]]}.wav')
+        shutil.copy(emodb_path + wav_file, emotion_path + f'/0_emodb_{EMOTIONS[emotion]}_{emotion_count[EMOTIONS[emotion]]}.wav')
+        emotion_count[EMOTIONS[emotion]] += 1
+
+
+if __name__ == '__main__':
+    prepare_emodb()
